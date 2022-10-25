858b232ca34c39e83acac4a7fd73d1fd5ef18a2d
李素兰 <8963476+li-sulan@user.noreply.gitee.com>
2022-10-25 21:19:55 +0800

    更新2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
     base: '/vuecomponents/',
     themeConfig: {
+      nav: [{
+        text: 'Home',link: '/'
+      },{
+        text: 'Github', link: 'https://github.com/lslppp/vuecomponents'
+      },{
+        text: 'Npm', link: 'https://www.npmjs.com/package/vuecardlisula'
+      }],
       sidebar: [
         '/',
         '/componentDocs/card'
